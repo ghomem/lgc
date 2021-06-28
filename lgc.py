6d72cfb103f0046fdb79492148f5c3473dbd3862
Gustavo Homem <gustavo@solidangle.eu>
2021-06-28 22:17:17 +0000

    implement confidence interval for risk ratio

--- lgc.py
+++ lgc.py
@@ -4,6 +4,7 @@ Present an interactive clinical trial dimensioning interface
 '''
 from binoculars import binomial_confidence
 from scipy import stats
+import math
 import numpy as np
 
 from bokeh.io import curdoc
@@ -25,21 +26,26 @@ TEST_START = 1000
 TEST_STEP  = 10
 
 # Proportions of positive events
-EVENTS_CONTROL_MIN   = 0
+EVENTS_CONTROL_MIN   = 0.25 # this can never be zero for the control group, otherwise the would be nothing to investigate
 EVENTS_CONTROL_MAX   = 100
 EVENTS_CONTROL_START = 5
-EVENTS_CONTROL_STEP  = 0.5
+EVENTS_CONTROL_STEP  = 0.25
 
 EVENTS_TEST_MIN   = 0
 EVENTS_TEST_MAX   = 100
 EVENTS_TEST_START = 2
-EVENTS_TEST_STEP  = 0.5
+EVENTS_TEST_STEP  = 0.25
 
 # Confidence level
 CI_MIN   = 90
 CI_MAX   = 99
 CI_START = 95
 CI_STEP  = 0.5
+
+# The Walter method is used when this variable is true. When it is false we use the simpler Katz method, but it crashes when the proportion for the test group is zero
+# Katz et al, 1978 and Walter, 1975 are summarized here:
+# https://www.jstor.org/stable/2531848
+WALTER_CI = True
 
 # labels and strings
 PAGE_TITLE  ='Clinical trial simulator'
@@ -62,11 +68,31 @@ TEXT_RESULTS = 'Inference from experimental results:'
 
 ### Functions
 
-def mk_risk_str ( risk, risk_l, risk_r ):
+def mk_risk_str ( title, risk, risk_l, risk_r ):
 
-    str_risk = 'Risk on control group (%) : ' + str(risk) + ' (' +  str(risk_l) + '-' + str(risk_r) + ')'
+    str_risk = title + str(risk) + ' (' +  str(risk_l) + '-' + str(risk_r) + ')'
 
     return str_risk
+
+def get_phi ( p0, p1, n0, n1, walter = False ):
+
+    if not walter:
+        return p1 / p0
+    else:
+        x0 = p0 * n0
+        x1 = p1 * n1
+        return math.exp( math.log( (x1 + 0.5) / (n1 + 0.5 ) ) - math.log( (x0 + 0.5) / (n0 + 0.5 ) ) )
+
+def get_par ( p0, p1, n0, n1, walter = False ):
+
+    if not walter:
+        par = math.sqrt( (1 - p0)/(n0 * p0) + (1 - p1)/(n1 * p1) )
+        return par
+    else:
+        x0 = p0 * n0
+        x1 = p1 * n1
+        par = math.sqrt( 1/(x1 + 0.5) - 1/(n1 + 0.5) + 1/(x0 + 0.5) - 1/(n0 + 0.5) )
+        return par
 
 # callback function for updating the data
 def update_data(attrname, old, new):
@@ -75,23 +101,32 @@ def update_data(attrname, old, new):
     z_value = stats.norm.isf( (1 - confidence_level) / 2 )
 
     # control group inference
-    control_risk    = events_control.value
+    control_risk    = round(events_control.value,2)
     control_risk_ci = np.array( binomial_confidence(control_risk / 100, control.value, z=z_value) ) * 100
     control_risk_l  = round(control_risk_ci[0], 2)
     control_risk_r  = round(control_risk_ci[1], 2)
 
-    str_control_risk = mk_risk_str (control_risk, control_risk_l, control_risk_r)
+    str_control_risk = mk_risk_str ('Risk on control group (%) : ', control_risk, control_risk_l, control_risk_r)
 
     # test group inference
-    test_risk    = events_test.value
+    test_risk    = round(events_test.value,2)
     test_risk_ci = np.array( binomial_confidence(test_risk / 100, test.value, z=z_value) ) * 100
     test_risk_l  = round(test_risk_ci[0], 2)
     test_risk_r  = round(test_risk_ci[1], 2)
 
-    str_test_risk = mk_risk_str (test_risk, test_risk_l, test_risk_r)
+    str_test_risk = mk_risk_str ('Risk on test group (%) &nbsp;&nbsp;&nbsp;&nbsp;&nbsp: ', test_risk, test_risk_l, test_risk_r)
 
     # risk ratio
-    str_risk_ratio = 'Risk ratio: ' + str ( round ( test_risk / control_risk, 2 ) )
+    # the form is phi * math.exp( +-z_value * parameter ) which is common to Katz and Walter methods
+
+    phi = get_phi (control_risk/100, test_risk/100, control.value, test.value, WALTER_CI)
+    par = get_par (control_risk/100, test_risk/100, control.value, test.value, WALTER_CI)
+
+    risk_ratio   = round(test_risk / control_risk,2)
+    risk_ratio_l = round(phi * math.exp( -z_value * par ),2)
+    risk_ratio_r = round(phi * math.exp( +z_value * par ),2)
+
+    str_risk_ratio = mk_risk_str ('Risk ratio : ', risk_ratio, risk_ratio_l, risk_ratio_r)
 
     # adverse effects threshold
     # to find at least one case at the current confidence level the probability must be this or higher
