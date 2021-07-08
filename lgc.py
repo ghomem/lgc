469b87a419cf0e3ad4cb6ac868844b81f940877c
Gustavo Homem <gustavo@solidangle.eu>
2021-07-08 09:04:44 +0000

    usability improvements

--- lgc.py
+++ lgc.py
@@ -19,15 +19,28 @@ PLOT_RANGE_DELTA = 5
 GROUPS = ['Test group', 'Control group']
 
 # Groups
+
+# coarse grain
 CONTROL_MIN   = 50
-CONTROL_MAX   = 25000
+CONTROL_MAX   = 20000
 CONTROL_START = 1000
 CONTROL_STEP  = 10
 
 TEST_MIN   = 50
-TEST_MAX   = 25000
+TEST_MAX   = 20000
 TEST_START = 1000
 TEST_STEP  = 10
+
+# fine grain
+CONTROL_FINE_MIN   = 0
+CONTROL_FINE_MAX   = 9
+CONTROL_FINE_START = 0
+CONTROL_FINE_STEP  = 1
+
+TEST_FINE_MIN   = 0
+TEST_FINE_MAX   = 9
+TEST_FINE_START = 0
+TEST_FINE_STEP  = 1
 
 # Proportions of positive events
 EVENTS_CONTROL_MIN   = 0.25 # this can never be zero for the control group, otherwise the would be nothing to investigate
@@ -61,6 +74,9 @@ PAGE_TITLE  ='Clinical trial simulator'
 
 CONTROL_LABEL = 'Control group size'
 TEST_LABEL    = 'Test group size'
+
+CONTROL_FINE_LABEL = 'Control group size fine tuning'
+TEST_FINE_LABEL    = 'Test group size fine tuning'
 
 EVENTS_CONTROL_LABEL = 'Detected proportion for the control group (%)'
 EVENTS_TEST_LABEL    = 'Detected proportion for the test group (%)'
@@ -151,10 +167,12 @@ def get_pvalue ( p0, p1, n0, n1 ):
 
     return round (p_value, 4)
 
+# return the pvalue for the risk ratio - 2
+#
 # Altman method, valid for 60 patients or more
 # https://www.bmj.com/content/bmj/343/bmj.d2304.full.pdf
 #
-# The purpose of this method is just trying to find the p-value if we don't have the detailed data.
+# The purpose of this method is just trying to find the p-value if we don't have the detailed data. It is here just for double checking purposes.
 # The value oscilates as little bit as the CI changes but remains around reasonable values
 #
 # test case:
@@ -191,6 +209,7 @@ def get_cvalue ( p0, p1, n0, n1, base_value, step ):
         current_confidence = base_value + j*step
         z_value = stats.norm.isf( (1 - current_confidence) / 2 )
         j = j +1
+
         # risk ratio
         # the form is phi * math.exp( +-z_value * parameter ) which is common to Katz and Walter methods
 
@@ -214,9 +233,16 @@ def update_data(attrname, old, new):
     confidence_level = ci.value / 100
     z_value = stats.norm.isf( (1 - confidence_level) / 2 )
 
+    control_participants = control.value + control_fine.value
+    test_participants    = test.value + test_fine.value
+
+    spacing = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
+    str_control_participants = 'Participants on control group: ' + str(control_participants)
+    str_test_participants    = 'Participants on test group' + spacing + ': ' + str(test_participants)
+
     # control group inference
     control_risk     = round(events_control.value,2)
-    control_risk_ci  = np.array( binomial_confidence(control_risk / 100, control.value, z=z_value, method=INDIVIDUAL_CI_METHOD) ) * 100
+    control_risk_ci  = np.array( binomial_confidence(control_risk / 100, control_participants, z=z_value, method=INDIVIDUAL_CI_METHOD) ) * 100
     control_risk_l   = round(control_risk_ci[0], 2)
     control_risk_r   = round(control_risk_ci[1], 2)
     control_risk_err = control_risk_ci[1] - control_risk_ci[0]
@@ -225,7 +251,7 @@ def update_data(attrname, old, new):
 
     # test group inference
     test_risk     = round(events_test.value,2)
-    test_risk_ci  = np.array( binomial_confidence(test_risk / 100, test.value, z=z_value, method=INDIVIDUAL_CI_METHOD) ) * 100
+    test_risk_ci  = np.array( binomial_confidence(test_risk / 100, test_participants, z=z_value, method=INDIVIDUAL_CI_METHOD) ) * 100
     test_risk_l   = round(test_risk_ci[0], 2)
     test_risk_r   = round(test_risk_ci[1], 2)
     test_risk_err = test_risk_ci[1] - test_risk_ci[0]
@@ -248,8 +274,8 @@ def update_data(attrname, old, new):
     # risk ratio
     # the form is phi * math.exp( +-z_value * parameter ) which is common to Katz and Walter methods
 
-    phi = get_phi (control_risk/100, test_risk/100, control.value, test.value, WALTER_CI)
-    par = get_par (control_risk/100, test_risk/100, control.value, test.value, WALTER_CI)
+    phi = get_phi (control_risk/100, test_risk/100, control_participants, test_participants, WALTER_CI)
+    par = get_par (control_risk/100, test_risk/100, control_participants, test_participants, WALTER_CI)
 
     risk_ratio   = round(test_risk / control_risk,2)
     risk_ratio_l = round(phi * math.exp( -z_value * par ),2)
@@ -259,24 +285,36 @@ def update_data(attrname, old, new):
 
     # adverse effects threshold
     # to find at least one case at the current confidence level the probability must be this or higher
-    adv_effects_threshold = (1 - ( 1 - confidence_level )**( 1 / test.value ) ) * 100
+    adv_effects_threshold = (1 - ( 1 - confidence_level )**( 1 / test_participants ) ) * 100
     str_adv_effects = 'Adverse effects detectability threshold (%): ' + str( round (adv_effects_threshold, 2) )
 
-    str_pvalue1 = 'p-value: ' + str( get_pvalue(control_risk/100, test_risk/100, control.value, test.value) )
-    str_pvalue2 = 'p-value2: ' + str( get_pvalue2(risk_ratio, risk_ratio_l, risk_ratio_r, confidence_level) )
+    p_value = get_pvalue(control_risk/100, test_risk/100, control_participants, test_participants)
 
-    #str_pvalue = str_pvalue1 + '<br/>' + str_pvalue2
+    indent = '&nbsp;&nbsp;'
+
+    if ( p_value > 0.0001 ):
+        str_pvalue1 = indent + 'p-value: ' + str(p_value)
+    else:
+        str_pvalue1 = indent + 'p-value: ' + '<= 0.0001'
+
     str_pvalue = str_pvalue1
 
-    c_value = get_cvalue ( control_risk/100, test_risk/100, control.value, test.value, 0.50, 0.005)
+    c_value = get_cvalue ( control_risk/100, test_risk/100, control_participants, test_participants, 0.50, 0.0001)
 
-    str_cvalue     = 'c-value: ' + str( c_value )
-    highest_ci    = round ((1 - c_value), 4)
-    str_cvalue_ext = 'highest CI: '  + str( highest_ci ) + ' / ' + str ( highest_ci*100 ) + '%'
+    highest_ci     = round ((1 - c_value), 4)
+    highest_ci_pct = round ((1 - c_value)*100,2)
 
-    text_risk.text        = '<br/>' + str_test_risk + '<br/>' + str_control_risk + '<br/><br/>' + str_overlap_interval + '<br/>' + str_overlap_pct_test
-    text_risk_ratio.text  = str_risk_ratio + '<br/>' + str_pvalue + '<br/>' + str_cvalue + '<br/>' + str_cvalue_ext
-    text_adv_effects.text = str_adv_effects
+    if ( c_value > 0.0001 ):
+        str_cvalue     = indent + 'c-value: ' + str( c_value )
+        str_cvalue_ext = indent + 'highest CI: ' + str( highest_ci ) + ' / ' + str ( highest_ci_pct ) + '%'
+    else:
+        str_cvalue     = indent + 'c-value: ' + '<= 0.0001'
+        str_cvalue_ext = indent + 'highest CI: ' + '>= 0.9999' + ' / ' + '>= 99.99%'
+
+    text_participants.text = '<br/>' + str_test_participants + '<br/>' + str_control_participants
+    text_risk.text         = str_test_risk + '<br/>' + str_control_risk + '<br/><br/>' + str_overlap_interval + '<br/>' + str_overlap_pct_test
+    text_risk_ratio.text   = str_risk_ratio + '<br/>' + str_pvalue + '<br/>' + str_cvalue + '<br/>' + str_cvalue_ext
+    text_adv_effects.text  = str_adv_effects
 
     # produce warnings in case they are necessary
     if 1 >= risk_ratio_l and 1 <= risk_ratio_r:
@@ -309,6 +347,9 @@ def reset_data():
     control.value = CONTROL_START
     test.value    = TEST_START
 
+    control_fine.value = CONTROL_FINE_START
+    test_fine.value    = TEST_FINE_START
+
     events_control.value = EVENTS_CONTROL_START
     events_test.value    = EVENTS_TEST_START
 
@@ -322,6 +363,9 @@ def reset_data():
 # Group size sliders
 control = Slider(title=CONTROL_LABEL, value=CONTROL_START, start=CONTROL_MIN, end=CONTROL_MAX, step=CONTROL_STEP)
 test    = Slider(title=TEST_LABEL, value=TEST_START, start=TEST_MIN, end=TEST_MAX, step=TEST_STEP)
+
+control_fine = Slider(title=CONTROL_FINE_LABEL, value=CONTROL_FINE_START, start=CONTROL_FINE_MIN, end=CONTROL_FINE_MAX, step=CONTROL_FINE_STEP)
+test_fine    = Slider(title=TEST_FINE_LABEL, value=TEST_FINE_START, start=TEST_FINE_MIN, end=TEST_FINE_MAX, step=TEST_FINE_STEP)
 
 # Detected proportions sliders
 events_control = Slider(title=EVENTS_CONTROL_LABEL, value=EVENTS_CONTROL_START, start=EVENTS_CONTROL_MIN, end=EVENTS_CONTROL_MAX, step=EVENTS_CONTROL_STEP)
@@ -339,10 +383,11 @@ text_intro   = Div(text=TEXT_INTRO)
 text_results = Div(text=TEXT_RESULTS)
 
 # dynamic labels
-text_risk        = Div(text='')
-text_risk_ratio  = Div(text='')
-text_adv_effects = Div(text='')
-text_warnings    = Div(text='')
+text_participants = Div(text='')
+text_risk         = Div(text='')
+text_risk_ratio   = Div(text='')
+text_adv_effects  = Div(text='')
+text_warnings     = Div(text='')
 
 # dummy values, they are updated after update_data runs
 test_risk   = 0
@@ -354,7 +399,6 @@ control_risk_l = 0
 control_risk_r = 0
 
 # Plot
-
 
 values = [ test_risk, control_risk     ]
 upper  = [ test_risk_r, control_risk_r ]
@@ -379,7 +423,7 @@ update_data('xxx', 0, 0)
 
 # Assign widgets to the call back function
 # updates are on value_throtled because this is too slow for realtime updates
-for w in [control, test, events_control, events_test, ci]:
+for w in [control, test, control_fine, test_fine, events_control, events_test, ci]:
     w.on_change('value_throttled', update_data)
 
 # reset button call back
@@ -389,8 +433,8 @@ left_margin   = Spacer(width=LMARGIN_WIDTH, height=400, width_policy='fixed', he
 middle_margin = Spacer(width=MMARGIN_WIDTH, height=400, width_policy='fixed', height_policy='auto')
 
 # layout
-inputs  = column(text_intro, test, control, events_test, events_control, ci, button)
-results = column(text_results, p, text_risk, text_risk_ratio, text_adv_effects, text_warnings)
+inputs  = column(text_intro, test, control, test_fine, control_fine, events_test, events_control, ci, button)
+results = column(text_results, p, text_participants, text_risk, text_risk_ratio, text_adv_effects, text_warnings)
 
 curdoc().title = PAGE_TITLE
 
